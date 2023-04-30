256589312adb8dec93809ae1c19ce87c536e6b6e
BuildTools <oze@obotach.com>
2023-04-30 13:30:27 -0400

    ticker endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 from fetchData import main as scrape
 from runner import process
+import pandas as pd
+import json
 
 app = FastAPI()
 
@@ -15,3 +17,8 @@ def read_root():
 def read_item(ticker: str, percentage: int = 50):
     scrape(ticker)
     return process(percentage)
+
+@app.get("/tickers")
+def read_tickers():
+    df = pd.read_csv("../ticker.txt", sep="\t", index_col=False)
+    return json.dumps(df.to_dict(orient='list')['ticker'])
